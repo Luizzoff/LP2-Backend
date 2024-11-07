54c418ea7cf7f027640c9703fc46721cbcd916a3
Luiz Gustavo Renner Scatalon <luizgustavo.zrs@hotmail.com>
2024-11-07 14:34:24 -0300

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -7,8 +7,8 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-const host = "localhost";
-const porta = 5000;
+const host = "0.0.0.0";
+const porta = 4000;
 const app = express();
 dotenv.config();
 
