8ee84fff1c5f2a3a121e6bc242a87350b3e08f8f
Luiz Gustavo Renner Scatalon <luizgustavo.zrs@hotmail.com>
2024-11-09 21:04:03 -0300

    url corrigida

--- frontend/src/services/servicoUsuario.js
+++ frontend/src/services/servicoUsuario.js
@@ -1,5 +1,5 @@
-// const url = 'https://luizzoff-lp2-backend.vercel.app/usuarios/';
-const url = 'http://localhost:5000/usuarios/'
+const url = 'https://luizzoff-lp2-backend.vercel.app/usuarios/';
+// const url = 'http://localhost:5000/usuarios/'
 
 export async function consultar(nick, senha) {
     const res = await fetch(url + nick + "/" + senha,{
